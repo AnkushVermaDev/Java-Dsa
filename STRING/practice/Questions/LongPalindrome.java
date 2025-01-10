1798c55b9f6554ed4e00b572f1de4c9ee3e3c24f
Ankush Verma <161600967+AnkushVermaDev@users.noreply.github.com>
2025-01-10 13:00:35 +0530

    Update LongPalindrome.java
    Easy way to find Longest Palindrome.

--- STRING/practice/Questions/LongPalindrome.java
+++ STRING/practice/Questions/LongPalindrome.java
@@ -1,42 +1,38 @@
-import java.util.*;
-
 public class LongPalindrome {
 
     public static void main(String[] args) {
 
-        String s = "thestewinciviclootsing";
+        String s = "fridaycivicavaaddams";
+        StringBuffer stb = new StringBuffer();
+        int maxfinder = 0;
+        String word = "";
+        String longestPalindrome = "";
 
-        String finalresult = "";
-        int maxlen = 0;
-
-        // Iterate through all possible centers (for both odd and even length
-        // palindromes)
         for (int i = 0; i < s.length(); i++) {
-            // Odd length palindrome (single character center)
-            String oddPalindrome = expandAroundCenter(s, i, i);
-            if (oddPalindrome.length() > maxlen) {
-                maxlen = oddPalindrome.length();
-                finalresult = oddPalindrome;
-            }
+            stb.setLength(0);
 
-            // Even length palindrome (pair of characters as center)
-            String evenPalindrome = expandAroundCenter(s, i, i + 1);
-            if (evenPalindrome.length() > maxlen) {
-                maxlen = evenPalindrome.length();
-                finalresult = evenPalindrome;
+            for (int j = i + 1; j < s.length(); j++) {
+
+                if (s.charAt(i) == s.charAt(j)) {
+                    word = s.substring(i, j + 1);
+                    stb.append(word);
+
+                    if (stb.reverse().toString().equals(word)) {
+
+                        if (word.length() > maxfinder) {
+                            maxfinder = word.length();
+                            longestPalindrome = word; // Update the longest palindrome
+                        }
+
+                    }
+
+                    // Reset stb for the next comparison
+                    stb.setLength(0);
+                }
             }
         }
 
-        System.out.println(maxlen);
-        System.out.println("the final result is: " + finalresult);
+        System.out.println("Longest Palindromic Substring: " + longestPalindrome);
     }
 
-    // Helper function to expand around the center
-    private static String expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-        return s.substring(left + 1, right);
-    }
 }
