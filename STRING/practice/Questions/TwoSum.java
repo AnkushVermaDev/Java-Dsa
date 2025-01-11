e9c01325a2efd67d107b91852fa306b127cec1eb
Ankush Verma <161600967+AnkushVermaDev@users.noreply.github.com>
2025-01-11 11:02:55 +0530

    Update TwoSum.java
    OPTIMISED WAY

--- STRING/practice/Questions/TwoSum.java
+++ STRING/practice/Questions/TwoSum.java
@@ -1,83 +1,25 @@
 import java.util.*;
-public class Main {
-	
-	public static int[] index(int left, int right,int[] arr)
-  {
-   
-   for(int i=0;i<arr.length;i++){
-   
-    if(arr[i]==left){
-    
-      left = i;
-    
-          }
-     else if(arr[i]==right){
-    
-      right = i;
 
+class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        // Create a HashMap to store the difference and its index
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // Iterate through the array
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            // Check if the complement exists in the map
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            // Add the current number and its index to the map
+            map.put(nums[i], i);
         }
-   
-   }
-   
-   return new int[] {left ,right};
-   
-   } 
-	
-	
-	
-	
-	
-	
-	
-	public static void main(String[] args) {
-		
-		
-		
-		int[] originalArray = {5, 3, 8, 6, 2};
 
-        // Copy the original array
-        int[] sortedArray = originalArray.clone();
-
-        // Sort the copied array
-        Arrays.sort(sortedArray);
-        
-        
-        
-        int left=0;
-        int target=10;
-        int right=originalArray.length-1;
-        while(left<right){
-        	
-        	if((sortedArray[left] + sortedArray[right]) ==target){
-        		System.out.println("got it");
-        		
-        	int[] result =	index( sortedArray[left], sortedArray[right],originalArray);
-        	System.out.println(result[0] + " " + result[1]);
-
-        	
-        		break;
-        		
-        		}
-        		
-        		
-        	else	if((sortedArray[left] + sortedArray[right]) > target){
-        		
-        		right--;
-        		
-        		}
-        		
-        		
-        		else	if((sortedArray[left] + sortedArray[right])<  target){
-        		
-        		left++;
-        		
-        		}
-        		
-        		
-        		
-        	
-        	
-        }
-        
-	}
+        // Return an empty array if no solution is found
+        return new int[] {};
+    }
 }
